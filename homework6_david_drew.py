2b3578c688c97f3d59f0bd629145869747bc4edf
djdrewrn-hub <djdrewrn@gmail.com>
2026-02-04 22:37:57 +0000

    start

--- homework6_david_drew.py
+++ homework6_david_drew.py
@@ -1,2 +1,8 @@
+# conditional lists
 web_users = ["Doc", "Grumpy", "Happy", "Sleepy", "Bashful"]
 new_users = ["Sneezy", "Dopey", "Happy", "Sleepy", "Bashful"]
+
+
+#nested dictionaries
+cities = {}
+    print (user.title)
