d4d291e5f624c3af8075cab3738245bcf29e42e6
djdrewrn-hub <djdrewrn@gmail.com>
2026-02-08 14:29:34 -0800

    Update new_users list to include 'Greg'

--- homework6_david_drew.py
+++ homework6_david_drew.py
@@ -1,6 +1,6 @@
 # conditional lists
 web_users = ["Doc", "Grumpy", "Happy", "Sleepy", "Bashful"]
-new_users = ["Sneezy", "Dopey", "Happy", "Sleepy", "Bashful"]
+new_users = ["Sneezy", "Dopey", "Greg", "Sleepy", "Bashful"]
 
 
 #nested dictionaries
