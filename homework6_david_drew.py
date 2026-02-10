3a34ac0a3ba8084af391b9e7524ece473045856d
djdrewrn-hub <djdrewrn@gmail.com>
2026-02-10 22:35:38 +0000

    testing

--- homework6_david_drew.py
+++ homework6_david_drew.py
@@ -2,10 +2,16 @@
 web_users = ['doc', 'grumpy', 'happy', 'sleepy', 'bashful']
 new_users = ['sneezy', 'dopey', 'greg', 'sleepy', 'bashful']
 
-if new_users == web_users:
-    print("This user name is already in use, please choose a different user name.")
-else:    
-    print("This user name is available.")  
+for user in new_users:
+    if user in web_users:
+        print ("This user name is already in use, please choose a different user name.")
+    else:
+        print ("This user name is available.")
+
+#if new_users in web_users:
+#    print(new_users.title() + "This user name is already in use, please choose a different user name.")
+#if new_users not in web_users:    
+#    print(new_users.title() + "This user name is available.")
 
 #nested dictionaries
 #cities = {}
