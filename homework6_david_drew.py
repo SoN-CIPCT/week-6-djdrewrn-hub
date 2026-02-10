eb646da7b69c2151aa2c1c76d6befe9c3ae71acd
djdrewrn-hub <djdrewrn@gmail.com>
2026-02-10 21:59:49 +0000

    if else

--- homework6_david_drew.py
+++ homework6_david_drew.py
@@ -1,8 +1,12 @@
 # conditional lists
-web_users = ["Doc", "Grumpy", "Happy", "Sleepy", "Bashful"]
-new_users = ["Sneezy", "Dopey", "Greg", "Sleepy", "Bashful"]
+web_users = ['doc', 'grumpy', 'happy', 'sleepy', 'bashful']
+new_users = ['sneezy', 'dopey', 'greg', 'sleepy', 'bashful']
 
+if new_users == web_users:
+    print("This user name is already in use, please choose a different user name.")
+else:    
+    print("This user name is available.")  
 
 #nested dictionaries
-cities = {}
-    print (user.title)
+#cities = {}
+ #   print (user.title)
