fb0069a94594d8d60610315e836c1f5e5b0b52b3
djdrewrn-hub <djdrewrn@gmail.com>
2026-02-12 20:26:57 +0000

    nested dictionaries

--- homework6_david_drew.py
+++ homework6_david_drew.py
@@ -9,5 +9,10 @@ for user in new_users:
         print (f"{user}. This user name is available.")
 
 #nested dictionaries
-#cities = {}
-#   print (user.title)
+cities = {}
+ 
+cities['New York City'] = {'Country': 'United States', 'Population': '8.4 million', 'Fact': 'The Big Apple'}
+cities['Tokyo'] = {'Country': 'Japan', 'Population': '9.2 million', 'Fact': 'The largest metropolitan area in the world'}
+cities['Paris'] = {'Country': 'France', 'Population': '2.1 million', 'Fact': 'The City of Light'}
+
+print(cities)
